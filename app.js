14653c3030fdaa5c631fa9456d55c22d68663fee
ddz <ddzyan@163.com>
2020-08-10 17:41:05 +0800

    优化代码

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ const assert = require('assert');
 
 // app.js
 module.exports = app => {
+  // 鉴权
   app.passport.verify(async (ctx, user) => {
-    // 检查用户
     assert(user.provider, 'user.provider should exists');
     assert(user.id, 'user.id should exists');
 
-    console.log(user);
     const { id } = user;
 
-    const existsUser = await ctx.model.User.findOne({ uuid: id });
+    const existsUser = await ctx.model.User.findOne({
+      where: { uuid: id },
+    });
     if (existsUser) {
       return existsUser;
     }
@@ -21,16 +22,20 @@ module.exports = app => {
     return newUser;
   });
 
-  // 将用户信息序列化后存进 session 里面，一般需要精简，只保存个别字段
+  // 将用户序列化后再存储到会话中
   app.passport.serializeUser(async (ctx, user) => {
     const { id, uuid, name, nickname, role_id } = user;
     return { id, uuid, name, nickname, role_id };
   });
 
-  // 反序列化后把用户信息从 session 中取出来，反查数据库拿到完整信息
+  // 从会话还原后反序列化用户
   app.passport.deserializeUser(async (ctx, user) => {
     if (user) {
-      const userRes = await ctx.model.User.findOne({ id: user.id });
+      const userRes = await ctx.model.User.findOne({
+        where: {
+          id: user.id,
+        },
+      });
       const { id, uuid, name, nickname, role_id, provider, avatar_url } = userRes;
       return {
         id, uuid, name, nickname, role_id, provider, avatar_url,
